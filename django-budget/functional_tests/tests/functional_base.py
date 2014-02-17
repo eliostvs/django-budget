0f68b74091a424b97dcbbb101fc555a6f220800d
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2014-02-17 00:23:42 -0300

    refactor funcional tests

--- django-budget/functional_tests/tests/functional_base.py
+++ django-budget/functional_tests/tests/functional_base.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.core.urlresolvers import reverse
 from django.test import LiveServerTestCase
 
 from splinter import Browser
@@ -7,23 +6,19 @@ from splinter import Browser
 User = get_user_model()
 
 
-class BaseLiveServerTestCase(LiveServerTestCase):
+class BaseLiveServer(LiveServerTestCase):
     username = 'spam'
     password = 'eggs'
 
     @classmethod
     def setUpClass(cls):
-        super(BaseLiveServerTestCase, cls).setUpClass()
+        super(BaseLiveServer, cls).setUpClass()
         cls.browser = Browser('phantomjs')
 
     @classmethod
     def tearDownClass(cls):
-        super(BaseLiveServerTestCase, cls).tearDownClass()
+        super(BaseLiveServer, cls).tearDownClass()
         cls.browser.quit()
-
-    def test_login_with_valid_user(self):
-        self.get_or_create_user()
-        self.login()
 
     def get_or_create_user(self):
         try:
@@ -41,5 +36,15 @@ class BaseLiveServerTestCase(LiveServerTestCase):
         self.browser.fill('password', self.password)
         self.browser.find_by_name('login').click()
 
-    def get_url(self, urlname):
-        return '%s%s' % (self.live_server_url, reverse(urlname))
+    def get_url(self, url):
+        return '%s/en/%s/' % (self.live_server_url, url)
+
+
+class LoginTestCase(BaseLiveServer):
+
+    def test_login_with_valid_user(self):
+        self.get_or_create_user()
+        self.login()
+        icon = self.browser.find_by_id('userIcon')
+
+        self.assertTrue(icon)
